01a559ce5d969f58b59d187e22467d80e67b2651
mohammedBnomr <moohaboy@gmail.com>
2021-05-22 15:52:01 +0300

    Add a snackbar using Material UI library

--- src/Palette.js
+++ src/Palette.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Snackbar from "@material-ui/core/Snackbar";
 import ColorBox from './ColorBox'
 import NavBar from "./NavBar";
 import "./Palette.css";
@@ -7,7 +8,8 @@ class Palette extends Component {
     super(props);
     this.state = {
       deg: 500,
-      format: "hex",
+        format: "hex",
+      open:false
     };
     this.changeColorDegree = this.changeColorDegree.bind(this);
     this.changeColorFormat = this.changeColorFormat.bind(this);
@@ -16,7 +18,9 @@ class Palette extends Component {
     this.setState({ deg: newDeg });
   }
   changeColorFormat(e) {
-    this.setState({ format: e.target.value });
+      this.setState({ format: e.target.value, open: true }, () => {
+        setTimeout(() => this.setState({ open: false }),1500);
+    });
   }
   render() {
     const colors = this.props.palette.colors[this.state.deg].map((color) => (
@@ -31,6 +35,15 @@ class Palette extends Component {
           changeFormat={this.changeColorFormat}
         />
         <div className="Palette-colors">{colors}</div>
+        <Snackbar
+          open={this.state.open}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                message={<span id="message-id">Format Changed To { this.state.format.toUpperCase()}</span>}
+        ContentProps={{
+            "aria-describedby":"message-id"
+        }}
+          autoHideDuration={1500}
+        />
       </div>
     );
   }
